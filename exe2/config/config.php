4136f30318721e75ff00fd3410e1ea5dc80e4360
Marcus Vinícius Passos dos Santos <61671870+marcuspassos013@users.noreply.github.com>
2024-01-02 11:22:50 -0300

    Add files via upload

---
+++ exe2/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+const SERVIDOR = "localhost";
+const BANCO    = "";//referente ao banco de dados
+const USUARIO  = "root";
+const SENHA    = "";
+
+
+const CONTROLLER_PADRAO = 'home';
+const METODO_PADRAO     = 'index';
+const NAMESPACE_CONTROLLER = 'app\\controllers\\';
+
+const URL_BASE = 'http://localhost/curso_css/exe2/';
